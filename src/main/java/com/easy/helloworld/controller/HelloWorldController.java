9f91056a3d5922be035e5a75e12988a0acc50b4b
tongli0804 <tongli1992@hotmail.com>
2022-09-09 15:27:23 +0800

    第一次提交

---
+++ src/main/java/com/easy/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.easy.helloworld.controller;
+
+import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.crypto.Mode;
+import cn.hutool.crypto.Padding;
+import cn.hutool.crypto.symmetric.AES;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/test")
+    public String test(String p1, String p2) {
+
+        AES aes = new AES(Mode.OFB, Padding.NoPadding, p1.getBytes(), "9875462154789536".getBytes());
+
+        String decryptStr = aes.decryptStr(p2, CharsetUtil.CHARSET_UTF_8);
+        return decryptStr;
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
